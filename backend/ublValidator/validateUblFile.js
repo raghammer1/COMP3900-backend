18acd4f0e82ac6c98498fa99be73c9e59f8b02ff
Raghav Agarwal <raghagarwal@gmail.com>
2024-06-26 22:34:10 +1000

    validation now working just need to setup UBL upload to DB and also PDF generation of the validation report

--- backend/ublValidator/validateUblFile.js
+++ backend/ublValidator/validateUblFile.js
@@ -1,13 +1,57 @@
-const path = require('path');
+// const path = require('path');
+
+// const validateUblFile = async (req, res) => {
+//   const filePath = path.join(__dirname, 'uploads', req.file.filename);
+//   const validationUrl =
+//     'https://services.ebusiness-cloud.com/ess-schematron/v1/web/validate/single?rules=Au-Nz%20peppol-1.0.10';
+
+//   try {
+//     const form = new FormData();
+//     form.append('file', fs.createReadStream(filePath), req.file.filename);
+
+//     const apiKey = await getToken(); // Get the current valid token
+
+//     const response = await axios.post(validationUrl, form, {
+//       headers: {
+//         Authorization: `Bearer ${apiKey}`,
+//         Accept: 'application/json',
+//         ...form.getHeaders(),
+//       },
+//     });
+
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(error.response ? error.response.status : 500).json({
+//       error: error.response ? error.response.data : error.message,
+//     });
+//   }
+// };
+
+// module.exports = validateUblFile;
+const axios = require('axios');
+const FormData = require('form-data');
+const { Readable } = require('stream');
+const { getToken } = require('./tokenService');
 
 const validateUblFile = async (req, res) => {
-  const filePath = path.join(__dirname, 'uploads', req.file.filename);
   const validationUrl =
     'https://services.ebusiness-cloud.com/ess-schematron/v1/web/validate/single?rules=Au-Nz%20peppol-1.0.10';
 
+  console.log('CAME HEREs');
   try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
+    }
+
     const form = new FormData();
-    form.append('file', fs.createReadStream(filePath), req.file.filename);
+    const fileStream = new Readable();
+    fileStream.push(req.file.buffer);
+    fileStream.push(null);
+
+    form.append('file', fileStream, {
+      filename: req.file.originalname,
+      contentType: req.file.mimetype,
+    });
 
     const apiKey = await getToken(); // Get the current valid token
 
@@ -19,7 +63,11 @@ const validateUblFile = async (req, res) => {
       },
     });
 
-    res.json(response.data);
+    console.log(
+      response.data.report.reports.AUNZ_PEPPOL_1_0_10.firedAssertionErrors
+    );
+
+    res.json(response);
   } catch (error) {
     res.status(error.response ? error.response.status : 500).json({
       error: error.response ? error.response.data : error.message,
