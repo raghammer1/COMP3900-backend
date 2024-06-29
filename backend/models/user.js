b2b48cc77682fa3e0264f29a4feb865e78848198
Raghav Agarwal <raghagarwal@gmail.com>
2024-06-29 16:44:24 +1000

    change in the user schema where now each object of pdf, ubl and validation in a user will be an object with a uniqueId from mongodb

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,32 @@
+// const mongoose = require('mongoose');
+
+// const pdfUblValidationSchema = new mongoose.Schema({
+//   pdfId: { type: mongoose.Schema.Types.ObjectId, ref: 'GridFS' },
+//   ublId: { type: mongoose.Schema.Types.ObjectId, ref: 'GridFS' },
+//   validatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'GridFS' },
+//   // name: { type: String, required: true },
+// });
+
+// const ublValidationSchema = new mongoose.Schema({
+//   ublId: { type: mongoose.Schema.Types.ObjectId, ref: 'GridFS' },
+//   validatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'GridFS' },
+//   // name: { type: String, required: true },
+// });
+
+// const userSchema = new mongoose.Schema({
+//   email: { type: String, unique: true },
+//   username: { type: String },
+//   password: { type: String },
+//   // Will be an array of objects containing ids of pdf, related ubl id and related validation id
+//   pdfUblValidation: [pdfUblValidationSchema],
+//   // Will be an array of objects containing ubl id and related validation id, this is for case where user just upload ubl for validation and not the pdf as well
+//   ublValidation: [ublValidationSchema],
+//   // pdfFiles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'uploads.files' }],
+// });
+
+// userSchema.index({ location: '2dsphere' });
+
+// module.exports = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 
 const pdfUblValidationSchema = new mongoose.Schema({
@@ -17,10 +46,10 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   username: { type: String },
   password: { type: String },
-  // Will be an array of objects containing ids of pdf, related ubl id and related validation id
-  pdfUblValidation: [pdfUblValidationSchema],
-  // Will be an array of objects containing ubl id and related validation id, this is for case where user just upload ubl for validation and not the pdf as well
-  ublValidation: [ublValidationSchema],
+  // Will be an object where keys are ObjectId strings and values are objects containing ids of pdf, related ubl id, and related validation id
+  pdfUblValidation: { type: Map, of: pdfUblValidationSchema },
+  // Will be an object where keys are ObjectId strings and values are objects containing ubl id and related validation id
+  ublValidation: { type: Map, of: ublValidationSchema },
   // pdfFiles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'uploads.files' }],
 });
 
