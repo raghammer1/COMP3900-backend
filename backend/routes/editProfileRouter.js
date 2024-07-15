c59691f6e2684fa4a87be049f43017c0528221cd
Raghav Agarwal <raghagarwal@gmail.com>
2024-07-15 15:48:10 +1000

    User can now successfully update the profile picture as well

--- backend/routes/editProfileRouter.js
+++ backend/routes/editProfileRouter.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const editProfileController = require('../editProfile/editProfileController');
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post(
   '/change-profile-photo',
   auth,
+  upload.single('file'),
   editProfileController.controllers.changeProfilePhoto
 );
 module.exports = router;
