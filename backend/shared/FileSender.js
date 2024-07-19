1818c25709ad57a2993af1b28a96e4a04f0d4a6e
Raghav Agarwal <raghagarwal@gmail.com>
2024-07-19 16:26:53 +1000

    backend-feature-trying-to-implement-api-for-sending

--- backend/shared/FileSender.js
+++ backend/shared/FileSender.js
@@ -1,3 +1,429 @@
+// // // require('dotenv').config();
+// // // const mongoose = require('mongoose');
+// // // const { getGridFSBucket } = require('../db');
+// // // const MailSender = require('./MailSender');
+// // // const user = require('../models/user');
+
+// // // const MY_EMAIL = process.env.MY_EMAIL;
+
+// // // // Helper function to retrieve a file by its ID
+// // // const getFileById = async (fileId) => {
+// // //   try {
+// // //     const _id = new mongoose.Types.ObjectId(fileId);
+// // //     const gfs = getGridFSBucket();
+// // //     const cursor = gfs.find({ _id });
+// // //     const files = await cursor.toArray();
+
+// // //     if (!files || files.length === 0) {
+// // //       throw new Error('File not found');
+// // //     }
+
+// // //     const file = files[0];
+// // //     const downloadStream = gfs.openDownloadStream(_id);
+
+// // //     let fileBuffer = Buffer.from('');
+// // //     for await (const chunk of downloadStream) {
+// // //       fileBuffer = Buffer.concat([fileBuffer, chunk]);
+// // //     }
+
+// // //     return {
+// // //       filename: file.filename,
+// // //       contentType: file.contentType,
+// // //       buffer: fileBuffer,
+// // //     };
+// // //   } catch (error) {
+// // //     throw new Error(
+// // //       `Error retrieving file with ID ${fileId}: ${error.message}`
+// // //     );
+// // //   }
+// // // };
+
+// // // const FileSender = async (req, res) => {
+// // //   const {
+// // //     email,
+// // //     xmlId,
+// // //     pdfId,
+// // //     validatorPdfId,
+// // //     message,
+// // //     emailSubject,
+// // //     sharedObjId,
+// // //     process,
+// // //     fileTypes,
+// // //     userId,
+// // //   } = req.body;
+
+// // //   const attachments = [];
+
+// // //   let htmlBody;
+// // //   if (!message) {
+// // //     htmlBody = '<div>By HexaHunks</div>';
+// // //   } else {
+// // //     htmlBody = `<div>${message}</div>`;
+// // //   }
+
+// // //   let sub;
+// // //   if (!emailSubject) {
+// // //     sub = 'Your Requested Files';
+// // //   } else {
+// // //     sub = emailSubject;
+// // //   }
+
+// // //   try {
+// // //     if (xmlId) {
+// // //       const xmlFile = await getFileById(xmlId);
+// // //       attachments.push({
+// // //         filename: xmlFile.filename,
+// // //         content: xmlFile.buffer,
+// // //         contentType: xmlFile.contentType,
+// // //       });
+// // //     }
+
+// // //     if (pdfId) {
+// // //       const pdfFile = await getFileById(pdfId);
+// // //       attachments.push({
+// // //         filename: pdfFile.filename,
+// // //         content: pdfFile.buffer,
+// // //         contentType: pdfFile.contentType,
+// // //       });
+// // //     }
+
+// // //     if (validatorPdfId) {
+// // //       const validatorPdfFile = await getFileById(validatorPdfId);
+// // //       attachments.push({
+// // //         filename: validatorPdfFile.filename,
+// // //         content: validatorPdfFile.buffer,
+// // //         contentType: validatorPdfFile.contentType,
+// // //       });
+// // //     }
+
+// // //     if (attachments.length === 0) {
+// // //       res.status(400).send({ message: 'No valid file IDs provided' });
+// // //     }
+
+// // //     if (!MY_EMAIL) {
+// // //       res.status(500).send({ message: `Server Error` });
+// // //     }
+
+// // //     const mailOptions = {
+// // //       from: MY_EMAIL,
+// // //       to: email,
+// // //       subject: sub,
+// // //       text: 'Please find your attached files @HexaHunks',
+// // //       html: htmlBody,
+// // //       attachments: attachments,
+// // //     };
+
+// // //     await MailSender(mailOptions);
+
+// // //     console.log(sharedObjId);
+// // //     const newHistoryObj = {
+// // //       email: email,
+// // //       subject: sub,
+// // //       fileTypes: fileTypes,
+// // //       process: process,
+// // //       sharedObjId,
+// // //     };
+
+// // //     const updatedUser = await user.findByIdAndUpdate(
+// // //       userId,
+// // //       { $push: { historyEmail: newHistoryObj } },
+// // //       { new: true, useFindAndModify: false }
+// // //     );
+
+// // //     res
+// // //       .status(200)
+// // //       .send({ message: 'Email sent with attachments successfully' });
+// // //   } catch (error) {
+// // //     console.error('Failed to send email with attachments:', error);
+// // //     res.status(400).send({ message: `Failed to send email: ${error.message}` });
+// // //   }
+// // // };
+
+// // // module.exports = FileSender;
+// // require('dotenv').config();
+// // const mongoose = require('mongoose');
+// // const { getGridFSBucket } = require('../db');
+// // const MailSender = require('./MailSender');
+// // const user = require('../models/user');
+
+// // const MY_EMAIL = process.env.MY_EMAIL;
+
+// // // Helper function to retrieve a file by its ID
+// // const getFileById = async (fileId) => {
+// //   try {
+// //     const _id = new mongoose.Types.ObjectId(fileId);
+// //     const gfs = getGridFSBucket();
+// //     const cursor = gfs.find({ _id });
+// //     const files = await cursor.toArray();
+
+// //     if (!files || files.length === 0) {
+// //       throw new Error('File not found');
+// //     }
+
+// //     const file = files[0];
+// //     const downloadStream = gfs.openDownloadStream(_id);
+
+// //     let fileBuffer = Buffer.from('');
+// //     for await (const chunk of downloadStream) {
+// //       fileBuffer = Buffer.concat([fileBuffer, chunk]);
+// //     }
+
+// //     return {
+// //       filename: file.filename,
+// //       contentType: file.contentType,
+// //       buffer: fileBuffer,
+// //     };
+// //   } catch (error) {
+// //     throw new Error(
+// //       `Error retrieving file with ID ${fileId}: ${error.message}`
+// //     );
+// //   }
+// // };
+
+// // const FileSender = async (req, res) => {
+// //   const {
+// //     email,
+// //     xmlId,
+// //     pdfId,
+// //     validatorPdfId,
+// //     message,
+// //     emailSubject,
+// //     sharedObjId,
+// //     process,
+// //     fileTypes,
+// //     userId,
+// //   } = req.body;
+
+// //   const attachments = [];
+
+// //   let htmlBody;
+// //   if (!message) {
+// //     htmlBody = '<div>By HexaHunks</div>';
+// //   } else {
+// //     htmlBody = `<div>${message}</div>`;
+// //   }
+
+// //   let sub;
+// //   if (!emailSubject) {
+// //     sub = 'Your Requested Files';
+// //   } else {
+// //     sub = emailSubject;
+// //   }
+
+// //   try {
+// //     if (xmlId) {
+// //       const xmlFile = await getFileById(xmlId);
+// //       attachments.push({
+// //         filename: xmlFile.filename,
+// //         content: xmlFile.buffer.toString('base64'),
+// //         contentType: xmlFile.contentType,
+// //       });
+// //     }
+
+// //     if (pdfId) {
+// //       const pdfFile = await getFileById(pdfId);
+// //       attachments.push({
+// //         filename: pdfFile.filename,
+// //         content: pdfFile.buffer.toString('base64'),
+// //         contentType: pdfFile.contentType,
+// //       });
+// //     }
+
+// //     if (validatorPdfId) {
+// //       const validatorPdfFile = await getFileById(validatorPdfId);
+// //       attachments.push({
+// //         filename: validatorPdfFile.filename,
+// //         content: validatorPdfFile.buffer.toString('base64'),
+// //         contentType: validatorPdfFile.contentType,
+// //       });
+// //     }
+
+// //     if (attachments.length === 0) {
+// //       return res.status(400).send({ message: 'No valid file IDs provided' });
+// //     }
+
+// //     if (!MY_EMAIL) {
+// //       return res.status(500).send({ message: `Server Error` });
+// //     }
+
+// //     const mailOptions = {
+// //       from: MY_EMAIL,
+// //       to: email,
+// //       subject: sub,
+// //       text: 'Please find your attached files @HexaHunks',
+// //       html: htmlBody,
+// //       attachments: attachments,
+// //     };
+
+// //     await MailSender(mailOptions);
+
+// //     console.log(sharedObjId);
+// //     const newHistoryObj = {
+// //       email: email,
+// //       subject: sub,
+// //       fileTypes: fileTypes,
+// //       process: process,
+// //       sharedObjId,
+// //     };
+
+// //     const updatedUser = await user.findByIdAndUpdate(
+// //       userId,
+// //       { $push: { historyEmail: newHistoryObj } },
+// //       { new: true, useFindAndModify: false }
+// //     );
+
+// //     res
+// //       .status(200)
+// //       .send({ message: 'Email sent with attachments successfully' });
+// //   } catch (error) {
+// //     console.error('Failed to send email with attachments:', error);
+// //     res.status(400).send({ message: `Failed to send email: ${error.message}` });
+// //   }
+// // };
+
+// // module.exports = FileSender;
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// const { getGridFSBucket } = require('../db');
+// const MailSender = require('./MailSender');
+// const user = require('../models/user');
+
+// const MY_EMAIL = process.env.MY_EMAIL;
+
+// // Helper function to retrieve a file by its ID
+// const getFileById = async (fileId) => {
+//   try {
+//     const _id = new mongoose.Types.ObjectId(fileId);
+//     const gfs = getGridFSBucket();
+//     const cursor = gfs.find({ _id });
+//     const files = await cursor.toArray();
+
+//     if (!files || files.length === 0) {
+//       throw new Error('File not found');
+//     }
+
+//     const file = files[0];
+//     const downloadStream = gfs.openDownloadStream(_id);
+
+//     let fileBuffer = Buffer.from('');
+//     for await (const chunk of downloadStream) {
+//       fileBuffer = Buffer.concat([fileBuffer, chunk]);
+//     }
+
+//     return {
+//       filename: file.filename,
+//       contentType: file.contentType,
+//       buffer: fileBuffer,
+//     };
+//   } catch (error) {
+//     throw new Error(
+//       `Error retrieving file with ID ${fileId}: ${error.message}`
+//     );
+//   }
+// };
+
+// const FileSender = async (req, res) => {
+//   const {
+//     email,
+//     xmlId,
+//     pdfId,
+//     validatorPdfId,
+//     message,
+//     emailSubject,
+//     sharedObjId,
+//     process,
+//     fileTypes,
+//     userId,
+//   } = req.body;
+
+//   const attachments = [];
+
+//   let htmlBody;
+//   if (!message) {
+//     htmlBody = '<div>By HexaHunks</div>';
+//   } else {
+//     htmlBody = `<div>${message}</div>`;
+//   }
+
+//   let sub;
+//   if (!emailSubject) {
+//     sub = 'Your Requested Files';
+//   } else {
+//     sub = emailSubject;
+//   }
+
+//   try {
+//     if (xmlId) {
+//       const xmlFile = await getFileById(xmlId);
+//       attachments.push({
+//         filename: xmlFile.filename,
+//         content: xmlFile.buffer.toString('base64'),
+//         contentType: xmlFile.contentType,
+//       });
+//     }
+
+//     if (pdfId) {
+//       const pdfFile = await getFileById(pdfId);
+//       attachments.push({
+//         filename: pdfFile.filename,
+//         content: pdfFile.buffer.toString('base64'),
+//         contentType: pdfFile.contentType,
+//       });
+//     }
+
+//     if (validatorPdfId) {
+//       const validatorPdfFile = await getFileById(validatorPdfId);
+//       attachments.push({
+//         filename: validatorPdfFile.filename,
+//         content: validatorPdfFile.buffer.toString('base64'),
+//         contentType: validatorPdfFile.contentType,
+//       });
+//     }
+
+//     if (attachments.length === 0) {
+//       return res.status(400).send({ message: 'No valid file IDs provided' });
+//     }
+
+//     if (!MY_EMAIL) {
+//       return res.status(500).send({ message: `Server Error` });
+//     }
+
+//     const mailOptions = {
+//       from: MY_EMAIL,
+//       to: email,
+//       subject: sub,
+//       text: 'Please find your attached files @HexaHunks',
+//       html: htmlBody,
+//       attachments: attachments,
+//     };
+
+//     await MailSender(mailOptions);
+
+//     console.log(sharedObjId);
+//     const newHistoryObj = {
+//       email: email,
+//       subject: sub,
+//       fileTypes: fileTypes,
+//       process: process,
+//       sharedObjId,
+//     };
+
+//     const updatedUser = await user.findByIdAndUpdate(
+//       userId,
+//       { $push: { historyEmail: newHistoryObj } },
+//       { new: true, useFindAndModify: false }
+//     );
+
+//     res
+//       .status(200)
+//       .send({ message: 'Email sent with attachments successfully' });
+//   } catch (error) {
+//     console.error('Failed to send email with attachments:', error);
+//     res.status(400).send({ message: `Failed to send email: ${error.message}` });
+//   }
+// };
+
+// module.exports = FileSender;
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { getGridFSBucket } = require('../db');
@@ -73,7 +499,7 @@ const FileSender = async (req, res) => {
       const xmlFile = await getFileById(xmlId);
       attachments.push({
         filename: xmlFile.filename,
-        content: xmlFile.buffer,
+        content: xmlFile.buffer.toString('base64'),
         contentType: xmlFile.contentType,
       });
     }
@@ -82,7 +508,7 @@ const FileSender = async (req, res) => {
       const pdfFile = await getFileById(pdfId);
       attachments.push({
         filename: pdfFile.filename,
-        content: pdfFile.buffer,
+        content: pdfFile.buffer.toString('base64'),
         contentType: pdfFile.contentType,
       });
     }
@@ -91,17 +517,17 @@ const FileSender = async (req, res) => {
       const validatorPdfFile = await getFileById(validatorPdfId);
       attachments.push({
         filename: validatorPdfFile.filename,
-        content: validatorPdfFile.buffer,
+        content: validatorPdfFile.buffer.toString('base64'),
         contentType: validatorPdfFile.contentType,
       });
     }
 
     if (attachments.length === 0) {
-      res.status(400).send({ message: 'No valid file IDs provided' });
+      return res.status(400).send({ message: 'No valid file IDs provided' });
     }
 
     if (!MY_EMAIL) {
-      res.status(500).send({ message: `Server Error` });
+      return res.status(500).send({ message: `Server Error` });
     }
 
     const mailOptions = {
--- backend/shared/MailSender.js
+++ backend/shared/MailSender.js
@@ -1,17 +1,203 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+// // // require('dotenv').config();
+// // // const nodemailer = require('nodemailer');
 
-const MailSender = async (mailOptions) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
+// // // const MailSender = async (mailOptions) => {
+// // //   const transporter = nodemailer.createTransport({
+// // //     service: 'gmail',
+// // //     auth: {
+// // //       user: process.env.MY_EMAIL,
+// // //       pass: process.env.MY_PASSWORD,
+// // //     },
+// // //   });
+
+// // //   await transporter.sendMail(mailOptions);
+// // //   console.log('Email sent successfully');
+// // // };
+
+// // // module.exports = MailSender;
+// // require('dotenv').config();
+// // const { google } = require('googleapis');
+// // const nodemailer = require('nodemailer');
+// // const { OAuth2 } = google.auth;
+
+// // const createTransporter = async () => {
+// //   const oauth2Client = new OAuth2(
+// //     process.env.CLIENT_ID,
+// //     process.env.CLIENT_SECRET,
+// //     'https://developers.google.com/oauthplayground'
+// //   );
+
+// //   oauth2Client.setCredentials({
+// //     refresh_token: process.env.REFRESH_TOKEN,
+// //   });
+
+// //   try {
+// //     // const accessToken = await oauth2Client.getAccessToken();
+
+// //     const transporter = nodemailer.createTransport({
+// //       service: 'gmail',
+// //       auth: {
+// //         type: 'OAuth2',
+// //         user: process.env.MY_EMAIL,
+// //         accessToken: process.env.ACCESS_TOKEN,
+// //         clientId: process.env.CLIENT_ID,
+// //         clientSecret: process.env.CLIENT_SECRET,
+// //         refreshToken: process.env.REFRESH_TOKEN,
+// //       },
+// //     });
+
+// //     return transporter;
+// //   } catch (error) {
+// //     console.error('Error creating access token:', error);
+// //     throw new Error('Failed to create access token');
+// //   }
+// // };
+
+// // const MailSender = async (mailOptions) => {
+// //   try {
+// //     const emailTransporter = await createTransporter();
+// //     await emailTransporter.sendMail(mailOptions);
+// //     console.log('Email sent successfully');
+// //   } catch (error) {
+// //     console.error('Failed to send email:', error);
+// //     throw new Error('Failed to send email');
+// //   }
+// // };
+
+// // module.exports = MailSender;
+// require('dotenv').config();
+// const { google } = require('googleapis');
+// const { OAuth2 } = google.auth;
+
+// const oauth2Client = new OAuth2(
+//   process.env.CLIENT_ID,
+//   process.env.CLIENT_SECRET,
+//   process.env.REDIRECT_URI
+// );
+
+// oauth2Client.setCredentials({
+//   refresh_token: process.env.REFRESH_TOKEN,
+//   access_token: process.env.ACCESS_TOKEN,
+// });
+
+// const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+
+// const sendEmailUsingGmailAPI = async (mailOptions) => {
+//   const email = [
+//     `To: ${mailOptions.to}`,
+//     `Subject: ${mailOptions.subject}`,
+//     'Content-Type: text/html; charset=UTF-8',
+//     'MIME-Version: 1.0',
+//     '',
+//     `${mailOptions.html || mailOptions.text}`,
+//   ].join('\n');
+
+//   const encodedEmail = Buffer.from(email)
+//     .toString('base64')
+//     .replace(/\+/g, '-')
+//     .replace(/\//g, '_')
+//     .replace(/=+$/, '');
+
+//   const res = await gmail.users.messages.send({
+//     userId: 'me',
+//     requestBody: {
+//       raw: encodedEmail,
+//     },
+//   });
+
+//   console.log('Email sent successfully:', res.data);
+// };
+
+// const MailSender = async (mailOptions) => {
+//   try {
+//     await sendEmailUsingGmailAPI(mailOptions);
+//     console.log('Email sent successfully');
+//   } catch (error) {
+//     console.error('Failed to send email:', error);
+//     throw new Error('Failed to send email');
+//   }
+// };
+
+// module.exports = MailSender;
+require('dotenv').config();
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+  access_token: process.env.ACCESS_TOKEN,
+});
+
+const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+
+const createEmailWithAttachments = (mailOptions) => {
+  const boundary = 'boundary';
+  const emailParts = [];
+
+  emailParts.push(
+    `To: ${mailOptions.to}`,
+    `Subject: ${mailOptions.subject}`,
+    'MIME-Version: 1.0',
+    `Content-Type: multipart/mixed; boundary=${boundary}`,
+    '',
+    `--${boundary}`,
+    'Content-Type: text/html; charset=UTF-8',
+    'Content-Transfer-Encoding: 7bit',
+    '',
+    `${mailOptions.html || mailOptions.text}`,
+    ''
+  );
+
+  mailOptions.attachments.forEach((attachment) => {
+    emailParts.push(
+      `--${boundary}`,
+      `Content-Type: ${attachment.contentType}`,
+      'Content-Transfer-Encoding: base64',
+      `Content-Disposition: attachment; filename="${attachment.filename}"`,
+      '',
+      attachment.content,
+      ''
+    );
+  });
+
+  emailParts.push(`--${boundary}--`);
+
+  return emailParts.join('\r\n');
+};
+
+const sendEmailUsingGmailAPI = async (mailOptions) => {
+  const rawEmail = createEmailWithAttachments(mailOptions);
+
+  const encodedEmail = Buffer.from(rawEmail)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+
+  const res = await gmail.users.messages.send({
+    userId: 'me',
+    requestBody: {
+      raw: encodedEmail,
     },
   });
 
-  await transporter.sendMail(mailOptions);
-  console.log('Email sent successfully');
+  console.log('Email sent successfully:', res.data);
+};
+
+const MailSender = async (mailOptions) => {
+  try {
+    await sendEmailUsingGmailAPI(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    throw new Error('Failed to send email');
+  }
 };
 
 module.exports = MailSender;
