4b61fb0ee62283f5e1009b7118c9e91a63af706c
Raghav Agarwal <raghagarwal@gmail.com>
2024-07-30 13:45:21 +1000

    google id fixed

--- backend/auth/forgotPassword.js
+++ backend/auth/forgotPassword.js
@@ -53,6 +53,9 @@ const forgotPassword = async (req, res) => {
     if (!User) {
       emailHTML =
         '<h1>You are not a hex member yet, please sign in to continue...</h1> <p>http://localhost:3000/register</p>';
+    } else if (User.googleId) {
+      emailHTML =
+        '<h1>You are a hex member using google account, please log in using google to continue...</h1> <p>http://localhost:3000/login</p>';
     } else {
       emailHTML = `<div><img src="https://images.pexels.com/photos/15107263/pexels-photo-15107263/free-photo-of-night-sky-above-the-trees.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"/><h1>Your OTP</h1><p>Please reset your password from here: <strong> <a href="${resetLink}">Reset Password</a></strong></p></div>`;
     }
