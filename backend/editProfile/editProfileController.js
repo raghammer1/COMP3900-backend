8c06951d9c48ec6bd3a83816e659e3103d6b9369
Raghav Agarwal <raghagarwal@gmail.com>
2024-08-04 12:03:07 +1000

    edit cometing done

--- backend/editProfile/editProfileController.js
+++ backend/editProfile/editProfileController.js
@@ -1,6 +1,7 @@
 const changeProfilePhoto = require('./changeProfilePhoto.js');
 const changeUsername = require('./changeUsername.js');
 
+// edit controllers
 exports.controllers = {
   changeProfilePhoto,
   changeUsername,
